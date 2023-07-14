f28b24d94b0cf230f196c27ec98182db8dff6b75
Thiago Rivaben Lopes <58109483+Thiago-177@users.noreply.github.com>
2023-07-14 16:41:35 -0300

    Commit1

---
+++ aula17/objeto01.js
@@ -0,0 +1,17 @@
+/*let amigo = {nome: 'José', 
+sexo: "M", 
+peso: 85.4,
+engordar(p) {
+    console.log('Engordou')
+    this.peso += p
+}}
+amigo.engordar(100) // isso é o exemplo de poder colocar funções dentro de variáveis o que diversifica muito a possibilidades de programar
+console.log(`${amigo.nome} pesa ${amigo.peso} Kg`)*/
+
+let s = 'JavaScript'
+let a = s.toUpperCase()
+console.log(a)
+let num = 8
+num**=2
+
+console.log(num)
